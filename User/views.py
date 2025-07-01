f09c7d7d23ce84ece26195731cf2b7749d104289
aminodin <aminakbari.young2021@gmail.com>
2025-07-01 20:49:57 +0330

    feat: improve `LoginAndRegisterView` to pass new contexts in GET method and handle more complcated logics in POST method
    - change `post` function to handle hidden inputs values
    - setup new messages for errors
    - create logout function logic

--- User/views.py
+++ User/views.py
@@ -1,124 +1,65 @@
 from django.shortcuts import render
-from django.contrib.auth import authenticate, login
-from User.forms import LoginForm
+from django.contrib.auth import authenticate, login, logout
+from User.forms import LoginForm , RegisterationForm
 from django.contrib import messages
 from django.shortcuts import redirect
-from django.http import HttpResponseRedirect
 from django.views import View
 
-
-# TODO:Use class base view (if you want) but it's so better if you separate the rendering funciton from login and registers processes
-    # TODO: Also you should use biult in djanfo forms. 
-# TODO: Are you sure the hidden input is a correct way ?
-# Using captcha can be done with this : https://django-simple-captcha.readthedocs.io/en/latest/usage.html#installation. before that , we need use buit in Forms and use class base view
-
 class LoginAndRegisterView(View):
-    form_class = LoginForm()
-    initial_form = {"loginForm" : form_class}
-    # TODO: Change the template name it self. it's a bad name
     template_name = 'login_and_register/login_index.html'
 
-    def get(self, request):
-        return render(request , self.template_name , self.initial_form)
+    def get(self ,request):
+        login_form = LoginForm()
+        registerForm = RegisterationForm()
+        
+        context = {
+            "login_form": login_form,
+            "register_form": registerForm
+        }
+
+        return render(request , self.template_name , context)
     
-    def post(self , request , *args , **kwargs):
-        print("post is started")
+    def post(self , request):
+        # Pass empty form if the input data have error
 
-        login_form =self.form_class
+        context = {
+            "login_form" : LoginForm(),
+            "register_form" : RegisterationForm()
+        }
 
-        if login_form.is_valid():
-            print("forms are valid")
-            username = login_form.username
-            password = login_form.password
+        # Use hidden input "action" for determine data type (register or login)
+        action = request.POST.get('action')
 
-            if 'confirmPassword' in request.POST:
-                print("this request is signup")
-                pass
-            elif 'username' in request.POST and 'password' in request.POST:
-                print("This request is login")
-                # Authenticate the user with the provided username and password
-                user = authenticate(username=username, password=password)
-                print("user authenticated")
-                
-                if user is None:
-                    print("User is none.")
-                    # Display an error message if authentication fails (invalid password)
-                    messages.error(request, "Invalid Password")
-                    return redirect('/login_register/')
-                else:
-                    print("user vaild. we loging it in ... ")
-                    # Log in the user and redirect to the home page upon successful login
-                    login(request, user)
-                    print("user logged in")
+        if action == "register":
+            register_form_includes_data = RegisterationForm(request.POST)
+            if register_form_includes_data.is_valid():
+                user = register_form_includes_data.save()
+                login(request , user)
+                # TODO: we can use f string for add user email in success message
+                messages.success(request,f"User {user.username} registered successfuly !")
+                return redirect('/')
+        elif action == "login":
+            requested_login_form = LoginForm(request.POST)
+            if requested_login_form.is_valid():
+                print("Valid")
+                username = requested_login_form.cleaned_data.get("username")
+                password = requested_login_form.cleaned_data.get("password")
+                authentication = authenticate(username=username , password=password)
+                if authentication is not None:
+                    login(request , authentication)
+                    # TODO: # TODO: we can use f string for add user email in info message
+                    messages.info(request , "Welcome Back!")
                     return redirect('/')
-                
-            else:
-                print("Form is not valid")
-                return redirect('/login_register')
+                else:
+                    messages.error(request , "Username or password is wrong")
+            # Pass the form with errors back to the template
+            context['login_form'] = requested_login_form
         else:
-            print("Form is not valid!")
-            print(f"The form is : {login_form}")
-        return render(request , self.template_name , self.initial_form)
+            #TODO: remove this printt statments or set error handler
+            print("Not valid")
+        return render(request , self.template_name , context)
 
-
-
-
-def login_and_register(request):
-
-    if request.user.is_authenticated:
-        return HttpResponseRedirect("/")
-    
-    # Check if the HTTP request method is POST (form submission)
-    if request.method == "POST" and request.POST.get('hidden') != 'signup':
-
-        username = request.POST.get('username')
-        password = request.POST.get('password')
-        
-        # Check if a user with the provided username exists
-        if not User.objects.filter(username=username).exists():
-            # Display an error message if the username does not exist
-            messages.error(request, 'Invalid Username')
-            return redirect('/login_and_register/')
-        
-        # Authenticate the user with the provided username and password
-        user = authenticate(username=username, password=password)
-        
-        if user is None:
-            # Display an error message if authentication fails (invalid password)
-            messages.error(request, "Invalid Password")
-            return redirect('/login_and_register/')
-        else:
-            # Log in the user and redirect to the home page upon successful login
-            login(request, user)
-            username = user.username
-            return redirect('/login_and_register/')
-        
-    elif request.method == "POST" and request.POST.get('hidden') == 'signup':
-        username = request.POST.get('username')
-        email = request.POST.get('email')
-        password = request.POST.get('password')
-        confirm_password = request.POST.get('confirm_password')
-        
-        # Check if a user with the provided username already exists
-        user = User.objects.filter(username=username)
-        
-        if user.exists():
-            # Display an information message if the username is taken
-            messages.info(request, "Username already taken!")
-            return redirect('/login_and_register/')
-        
-        # Create a new User object with the provided information
-        user = User.objects.create_user(
-            password=password,
-            username=username
-        )
-        
-        # Set the user's password and save the user object
-        user.set_password(password)
-        user.save()
-        
-        # Display an information message indicating successful account creation
-        messages.info(request, "Account created Successfully!")
-        return redirect('/login_and_register/')
-    
-    return render(request, 'login_and_register/login_index.html')
+def logout_view(request):
+    logout(request)
+    messages.error(request , "User logged out.")
+    return redirect('/') # Redirect to the home page after logout
