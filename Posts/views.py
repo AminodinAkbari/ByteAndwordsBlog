b1f6774f5b3092ba273715deb2d321c8ed7d7fe0
aminodin <aminakbari.young2021@gmail.com>
2025-07-26 19:13:00 +0330

    chore: change comment saving logic
    - set some new messages and use get_absolute_url

--- Posts/views.py
+++ Posts/views.py
@@ -2,11 +2,12 @@
 This file contains the views for the Posts app. one of the views is FBV (Function Based View) and the other one is CBV (Class Based View). this isn't have any reason I just wanted to use both of them.
 """
 
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 
 from Posts.models import Post
 from Comment.forms import CommentForm
 from Comment.models import CommentModel
+from django.contrib import messages
 
 from django.views.generic import DetailView
 
@@ -33,22 +34,39 @@ class PostDetailView(DetailView):
 
         post = self.object
 
-        context['comments'] = CommentModel.objects.filter(post=post).order_by('-created_at') # Example ordering
+        context['comments'] = CommentModel.objects.filter(post=post , is_approved=True).order_by('created_at') # Example ordering
         context['comment_form'] = CommentForm()
 
         return context
 
     def post(self, request, *args, **kwargs):
         # Handle POST request
-        if request.method == 'POST':
-            form_data = CommentForm(request.POST or None)
-            if form_data.is_valid():
-                print("form_data" , form_data)
-                CommentModel.objects.create(
-                    post = self.get_object(),
-                    content = form_data.cleaned_data['content'],
-                )
-                return self.get(request, *args, **kwargs)
+        # TODO: Move comment creation to Comment/views.py file.
+        
+        if not request.user.is_authenticated:
+            return redirect('LoginAndRegisterUrl')
+        
+        post = self.get_object()
+        form_data = CommentForm(request.POST)
+
+        if form_data.is_valid():
+            # Add new comment to the post but not save it yet (commit=False)
+            new_comment = form_data.save(commit=False)
+
+            # Assign current post and user to the new comment
+            new_comment.post = post
+            new_comment.author = request.user
+
+            # Save the new comment
+            new_comment.save()
+
+            messages.success(request, 'Your comment has been submitted and is awaiting approval.')
+
+            return redirect(post.get_absolute_url())
+
+        return redirect(post.get_absolute_url())
+
+
 
 
 """
