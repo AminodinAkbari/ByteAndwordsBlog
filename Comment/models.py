e290ec988a09f16499663e420c5e00561cf52945
aminodin <aminakbari.young2021@gmail.com>
2025-07-26 19:11:35 +0330

    chore: remove 'get_edit_url' function and ordering commets with 'created_at' field

--- Comment/models.py
+++ Comment/models.py
@@ -12,9 +12,10 @@ methods:
     - __str__: returns the content of the comment
     - get_edit_comment_url: returns the url for editing the comment
 """
-
+from django.urls import reverse
 from django.db import models
 from Posts.models import Post
+from django.contrib.auth.models import User
 
 # Create your models here.
 class CommentModel(models.Model):
@@ -22,15 +23,19 @@ class CommentModel(models.Model):
     This class defines the model for the comments section of the blog.
     """
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
-    # TODO: uncomment this line when you want to add the author field. (the `author` can't be disable)
-    # author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    # TODO: Change the model from User (from django.contrib.auth.models) to a custom user model if you create one (another user model in User/models.py)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True , null=True , blank=True)
+    is_approved = models.BooleanField(default=False , blank=False , null=False)
+
+    class Meta:
+        ordering = ['-created_at']
 
     def __str__(self):
         return self.content
 
     # TODO: Add edit comment for comments. (edit by author only)
-    def get_edit_url(self):
-        return reverse('edit_comment', args=[str(self.id)])
+    # def get_edit_url(self):
+    #     return reverse('edit_comment', args=[str(self.post.slug)])
