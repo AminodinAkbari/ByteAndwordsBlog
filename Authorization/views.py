e66c57b5a1753de90b5def16bd7c6e43ecc03b23
Aminodin <aminakbari.young2021@gmail.com>
2025-09-23 19:43:25 +0330

    feat(authorization): add current user API view
    Introduce CurrentUserAPI view for retrieving and updating authenticated user
    data with proper permissions and serializers.

--- Authorization/views.py
+++ Authorization/views.py
@@ -1,1 +1,17 @@
-""" Authorization views file """
+from rest_framework.generics import RetrieveUpdateAPIView
+from User.serializer import UsersListingSerializer, UserRegistrationSerializer
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+from rest_framework import status
+from rest_framework_simplejwt.views import TokenObtainPairView
+
+from Authorization.models import CustomAuthenticationUser
+
+# TODO: We have Authorization and User model. check we correct about it ? we should remove one of those ?
+
+class CurrentUserAPI(RetrieveUpdateAPIView):
+    permission_classes = [IsAuthenticated]
+    serializer_class = UsersListingSerializer
+
+    def get_object(self):
+        return self.request.user
