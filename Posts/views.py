800986229129b02f77a5a775eb86bd8f463bcd7b
Aminodin <aminakbari.young2021@gmail.com>
2025-09-27 21:54:54 +0330

    refactor(Post): convert post views to viewset with tag filtering
    This viewset replaces the previous ListAPIView and RetrieveAPIView with a
    ViewSet, consolidating post listing, retrieval by slug, and tag-based filtering
    into a single class. Pagination is now handled explicitly through the
    NormalResultsPagination class instead of relying on the default settings.

--- Posts/views.py
+++ Posts/views.py
@@ -3,44 +3,56 @@ This file contains the views for the Posts app. one of the views is FBV (Functio
 """
 
 # Posts/views.py
-from rest_framework import generics, permissions
+from rest_framework import viewsets, permissions
+from rest_framework.response import Response
 from django.db.models import Prefetch
+from django.shortcuts import get_object_or_404
 from Posts.models import Post
 from Comment.models import CommentModel
 # Import our new serializers
 from Posts.serializer import PostSerializer, PostDetailSerializer
 from Posts.pagination import NormalResultsPagination
 
-class PostListAPIView(generics.ListAPIView):
+class PostViewSet(viewsets.ViewSet):
+
+    """ Thsi view will do these things : list all posts, retrieve by slug ,list posts accourding to a tag and create new posts.
     """
-    API View to list all posts (only published posts).
-    """
-    # We only want to show published posts, just like in your original view
-    queryset = Post.objects.filter(status = 'published')
-    serializer_class = PostSerializer
-    # Anyone can view the list of posts
-    permission_classes = [permissions.AllowAny]
-    pagination_class = NormalResultsPagination
 
-class PostDetailRetrieveAPIView(generics.RetrieveAPIView):
-    """
-    Api view to see a post detail using it's slug (only published posts)
-    """
-    queryset = Post.objects.filter(status = 'published')
-    serializer_class = PostDetailSerializer
-    permission_classes = [permissions.AllowAny]
+    def list(self, request):
+        """
+        Return a list of all posts.
 
-    lookup_field = 'slug'
-    lookup_url_kwarg = 'post_slug'
+        This endpoint supports optional pagination through the `page_size` query parameter.
+        - If `page_size` is provided in the request URL (e.g. `?page_size=10`), results
+          are paginated using `NormalResultsPagination`, and the response will include
+          pagination metadata (count, next, previous, results).
+        - If `page_size` is not provided, all posts are returned in a single response
+          without pagination.
+        - ViewSet not support default pagination in settings file.
+        """
+        queryset = Post.objects.all()
+        paginator = NormalResultsPagination()
 
-    def get_queryset(self):
-        queryset = super().get_queryset()
-        available_comments = CommentModel.objects.filter(is_approved = True)
-        print("Comments :" ,available_comments)
+        pages = paginator.paginate_queryset(queryset , request , view=self)
 
-        return queryset.prefetch_related(
-            Prefetch('comments', queryset = available_comments)
-        )
+        if pages is not None:
+            serializer = PostSerializer(pages , many=True)
+            return paginator.get_paginated_response(serializer.data)
 
+        serializer = PostSerializer(queryset , many = True)
+        return Response(serializer.data)
 
-    #     return context
+    def retrieve(self , request , slug):
+        """ Get a post accourding to requested slug. """
+        queryset = Post.objects.all()
+        retrieved_post = get_object_or_404(queryset , slug = slug)
+        serializer = PostSerializer(retrieved_post)
+        return Response(serializer.data)
+
+    def retrieve_posts_by_tag(self , request, *args, **kwargs):
+        """Reveive all posts those have requested tag. """
+        tag = kwargs["tag"]
+        queryset = Post.objects.filter(tags__slug = tag)
+        serializer = PostSerializer(queryset , many = True)
+        return Response(serializer.data)
+
