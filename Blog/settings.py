f37bd8e4cc0c215758abeb8ee3bb942abecc010d
Aminodin <aminakbari.young2021@gmail.com>
2025-10-18 18:28:18 +0330

    chore(settings): remove staticfiles app and static config

--- Blog/settings.py
+++ Blog/settings.py
@@ -36,7 +36,6 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
     # not built in Apps:
     'Posts',
     'User',
@@ -139,12 +138,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / "media"
-
-STATIC_URL = 'static/'
-
-STATICFILES_DIRS = [
-    BASE_DIR / 'static'
-]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
