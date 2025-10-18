1df89c8b0ee60be53ae9054164cfd7b4860586b6
Aminodin <aminakbari.young2021@gmail.com>
2025-10-18 18:56:08 +0330

    feat(settings): configure static files handling
    Added 'django.contrib.staticfiles' to INSTALLED_APPS and set up STATIC_URL and
    STATICFILES_DIRS to manage static assets.
    This commit fix the built-in DRF static files lost.

--- Blog/settings.py
+++ Blog/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'django.contrib.staticfiles',
     # not built in Apps:
     'Posts',
     'User',
@@ -138,6 +139,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / "media"
+
+STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'static'
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
