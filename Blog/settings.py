de798f9aa366430c793dd32dba002dc0ae00bb2e
AminodinAkbari <aminakbari.young2021@gmail.com>
2025-12-12 22:18:54 +0330

    chore(database): switch to PostgreSQL
    Updated database configuration to use PostgreSQL instead of SQLite and added
    required dependency psycopg2-binary

--- Blog/settings.py
+++ Blog/settings.py
@@ -91,9 +91,13 @@ WSGI_APPLICATION = 'Blog.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    'default' : {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'mydb',
+        'USER': 'myuser',
+        'PASSWORD': 'mypass',
+        'HOST': 'localhost',
+        'PORT': '5433',
     }
 }
 
